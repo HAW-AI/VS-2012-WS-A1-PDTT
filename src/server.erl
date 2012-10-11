32318eaa0a06c2aa145ba975fe03a7b961a6e820
Patrick Detlefsen <patrick.detlefsen@gmail.com>
2012-10-11 15:48:01 +0200

    use a state record instead of just a config variable to carry state through the loop

--- src/server.erl
+++ src/server.erl
@@ -2,29 +2,31 @@
 -import(werkzeug, [get_config_value/2,logging/2,timeMilliSecond/0,delete_last/1]).
 
 -compile([export_all]).
+-record(state, {config,
+                current_message_number=0}).
 
 start() ->
   {ok, Config} = file:consult("../server.cfg"),
-  Server = spawn(fun() -> loop(Config) end),
+  Server = spawn(fun() -> loop(State = #state{config=Config}) end),
   logging("server.log", io_lib:format("Server Startzeit: ~p mit PID ~p ~n", [timeMilliSecond(), Server])),
   Server.
 
-loop(Config) ->
-  {ok, Lifetime} = orddict:find(lifetime, Config),
+loop(State) ->
+  {ok, Lifetime} = orddict:find(lifetime, State#state.config),
 
   receive
     {getmessages, PID} ->
       logging("server.log", io_lib:format("Get messages from PID: ~p ~n", [PID])),
-      loop(Config);
+      loop(State);
     {dropmessage, {Message, Number}} ->
       logging("server.log", io_lib:format("Drop message {~p , ~p}~n", [Message, Number])),
-      loop(Config);
+      loop(State);
     {getmsgeid, PID} ->
       logging("server.log", io_lib:format("Get message ID ~p ~n", [PID])),
-      loop(Config);
+      loop(State);
     Unknown ->
       logging("server.log", io_lib:format("Got unknown Message ~p ~n", [Unknown])),
-      loop(Config)
+      loop(State)
   after Lifetime * 1000 ->
     logging("server.log", io_lib:format("Server Lifetime timeout after: ~p seconds ~n", [Lifetime]))
   end.
