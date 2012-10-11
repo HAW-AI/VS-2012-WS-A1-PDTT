c353d4e4be4459a033f6458f037a523fd1434cc8
Patrick Detlefsen <patrick.detlefsen@gmail.com>
2012-10-11 14:40:12 +0200

    keep server going after one request. return the correct PID in log upon starting the server. deal with unknown messages with a catch-all match

--- src/server.erl
+++ src/server.erl
@@ -4,24 +4,25 @@
 -compile([export_all]).
 
 start() ->
-  logging("server.log", io_lib:format("Server Startzeit: ~p mit PID ~p ~n", [timeMilliSecond(), self()])),
-  spawn(fun() -> loop() end).
+  Server = spawn(fun() -> loop() end),
+  logging("server.log", io_lib:format("Server Startzeit: ~p mit PID ~p ~n", [timeMilliSecond(), Server])),
+  Server.
 
 loop() ->
   receive
     {getmessages, PID} ->
-      logging("server.log", io_lib:format("Get messages from PID: ~p ~n", [PID]));
+      logging("server.log", io_lib:format("Get messages from PID: ~p ~n", [PID])),
+      loop();
     {dropmessage, {Message, Number}} ->
-      logging("server.log", io_lib:format("Drop message {~p , ~p}~n", [Message, Number]));
+      logging("server.log", io_lib:format("Drop message {~p , ~p}~n", [Message, Number])),
+      loop();
     {getmsgeid, PID} ->
-      logging("server.log", io_lib:format("Get message ID ~p ~n", [PID]))
-
-
+      logging("server.log", io_lib:format("Get message ID ~p ~n", [PID])),
+      loop();
+    Unknown ->
+      logging("server.log", io_lib:format("Got unknown Message ~p ~n", [Unknown])),
+      loop()
   end.
-
-pid() ->
-  self().
-
 
 stop() ->
     ok.
