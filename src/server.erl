3c80cc7aef246b6a0b294ea27343407e10df6284
Patrick Detlefsen <patrick.detlefsen@gmail.com>
2012-10-11 15:10:35 +0200

    added the server conf and the server now times out after the specified amount of time

--- src/server.erl
+++ src/server.erl
@@ -4,24 +4,29 @@
 -compile([export_all]).
 
 start() ->
-  Server = spawn(fun() -> loop() end),
+  {ok, Config} = file:consult("../server.cfg"),
+  Server = spawn(fun() -> loop(Config) end),
   logging("server.log", io_lib:format("Server Startzeit: ~p mit PID ~p ~n", [timeMilliSecond(), Server])),
   Server.
 
-loop() ->
+loop(Config) ->
+  {ok, Lifetime} = orddict:find(lifetime, Config),
+
   receive
     {getmessages, PID} ->
       logging("server.log", io_lib:format("Get messages from PID: ~p ~n", [PID])),
-      loop();
+      loop(Config);
     {dropmessage, {Message, Number}} ->
       logging("server.log", io_lib:format("Drop message {~p , ~p}~n", [Message, Number])),
-      loop();
+      loop(Config);
     {getmsgeid, PID} ->
       logging("server.log", io_lib:format("Get message ID ~p ~n", [PID])),
-      loop();
+      loop(Config);
     Unknown ->
       logging("server.log", io_lib:format("Got unknown Message ~p ~n", [Unknown])),
-      loop()
+      loop(Config)
+  after Lifetime * 1000 ->
+    logging("server.log", io_lib:format("Server Lifetime timeout after: ~p seconds ~n", [Lifetime]))
   end.
 
 stop() ->
